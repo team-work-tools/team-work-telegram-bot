214df1350f0722a34c05ba52574a55ab53f971a4
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-05-08 19:38:06 +0300

    fix(flake): add more 'follows'

--- flake.nix
+++ flake.nix
@@ -3,7 +3,26 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell.url = "github:deemp/devshell";
+    devshell = {
+      url = "github:deemp/devshell";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.inputs.systems.follows = "systems";
+      };
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs = {
+        flake-utils.follows = "devshell/flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
   };
 
   outputs =
