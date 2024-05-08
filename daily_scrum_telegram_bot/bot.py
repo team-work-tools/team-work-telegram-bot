f52682e88ec8d41e8738d06a2e500cc71509f187
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-05-08 22:34:26 +0300

    refactor(bot): move bot code to a separate module

---
+++ daily_scrum_telegram_bot/bot.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+from aiogram import Bot, Dispatcher, Router
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from . import handlers
+from .state import State, save_state
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from .state import load_state
+from .middlewares import HasScheduler, HasSendMessage, HasState
+from .meeting import schedule_meeting
+from .custom_types import SendMessage
+from .constants import EnvVariables, BotCommands, BotDataFiles
+
+dp = Dispatcher()
+
+
+def get_variable_name(expr: str) -> str:
+    return expr.split("=")[0]
+
+
+def init_state(state_file: Path) -> State:
+    if not state_file.exists():
+        state = State()
+        save_state(state, state_file)
+        return state
+    return load_state(state_file)
+
+
+def add_middlewares(
+    router: Router,
+    state_file: Path,
+    scheduler: AsyncIOScheduler,
+    send_message: SendMessage,
+) -> None:
+    router.message.middleware(
+        HasState(
+            load_state=lambda: load_state(state_file=state_file),
+            save_state=lambda state: save_state(state=state, state_file=state_file),
+        )
+    )
+
+    router.message.middleware(HasScheduler(scheduler=scheduler))
+
+    router.message.middleware(HasSendMessage(send_message=send_message))
+
+
+async def main(bot_token: str, bot_data_directory: str) -> None:
+    if bot_token is None:
+        raise ValueError(
+            f"The '{EnvVariables.BOT_TOKEN}' environment variable is not set"
+        )
+    if bot_data_directory is None:
+        raise ValueError(
+            f"The '{EnvVariables.BOT_DATA_DIRECTORY}' environment variable is not set"
+        )
+
+    state_file = Path(bot_data_directory) / BotDataFiles.state_file
+    state_file.parent.mkdir(parents=True, exist_ok=True)
+    state = init_state(state_file=state_file)
+
+    scheduler = AsyncIOScheduler()
+    scheduler.start()
+
+    bot = Bot(token=bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    async def send_message(message: str, state: State):
+        # Should send a message to the most recent chat_id
+        match state.chat_id:
+            # TODO handle missing chat_id somehow
+            case None:
+                print(
+                    f"Could not retrive a chat id. Please add the bot to a chat and run /{BotCommands.start}"
+                )
+            case _:
+                return await bot.send_message(chat_id=state.chat_id, text=message)
+
+    if state.meeting_time and state.chat_id:
+        schedule_meeting(
+            meeting_time=state.meeting_time,
+            scheduler=scheduler,
+            load_state=lambda: load_state(state_file=state_file),
+            save_state=lambda state: save_state(state=state, state_file=state_file),
+            send_message=send_message,
+        )
+
+    add_middlewares(
+        router=handlers.router,
+        state_file=state_file,
+        scheduler=scheduler,
+        send_message=send_message,
+    )
+
+    dp.include_router(handlers.router)
+
+    # drop all incoming messages
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    # start bot
+    await dp.start_polling(bot)
