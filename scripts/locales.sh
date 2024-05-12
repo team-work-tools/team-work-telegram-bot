a50b9f0ba4122f985febb1b71654a4c84a14277b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-05-12 23:56:26 +0300

    feat(scripts): add a script to generate locales files

---
+++ scripts/locales.sh
@@ -0,0 +1,12 @@
+# shellcheck disable=SC2148
+
+# extract messages
+poetry run pybabel extract --input-dirs=. -o locales/messages.pot
+
+# update localized messages
+for lang in en ru; do
+  poetry run pybabel update -i locales/messages.pot -d locales -D messages -l $lang
+done
+
+# Compile translations
+pybabel compile -d locales -D messages
