e69715b10c49da6b458e03706056ae209c7b9e6e
fullerite <sh.ramil2005@gmail.com>
2024-06-16 17:00:13 +0300

    Create User collection

--- bot/state.py
+++ bot/state.py
@@ -3,10 +3,39 @@ from typing import Annotated, Optional
 from zoneinfo import ZoneInfo
 
 import pymongo
-from beanie import Document, Indexed
+from beanie import Document, Link, Indexed
 
 from .chat import ChatId
 from .language import Language
+
+
+class User(Document):
+    username: Annotated[str, Indexed(unique=True)]
+    work_days: set[int] = set()
+
+    def __hash__(self):
+        return hash(self.id)
+    
+    def __eq__(self, other):
+        if isinstance(other, User):
+            return self.id == other.id
+        return False
+
+
+async def create_user(username: str) -> User:
+    return await User(username=username).create()
+
+
+async def load_user(username: str) -> User:
+    match user := await User.find_one(User.username == username):
+        case User():
+            return user
+        case _:
+            return await create_user(username=username)
+        
+
+async def save_user(user: User) -> None:
+    await user.save()
 
 
 class ChatState(Document):
