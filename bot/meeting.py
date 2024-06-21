38f534e3deb7f87343bdb3e25c219122474612a8
N. Adagamov <adagamov05@mail.ru>
2024-06-21 22:58:14 +0300

    Record meeting days' ids into db

--- bot/meeting.py
+++ bot/meeting.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from .constants import day_of_week, jobstore
 from .custom_types import ChatId, SendMessage
 from .messages import make_daily_messages
-from .state import ChatState, load_state, get_joined_users
+from .state import load_state, save_state, get_joined_users
 
 
 async def send_meeting_messages(chat_id: ChatId, send_message: SendMessage):
@@ -22,9 +22,19 @@ async def send_meeting_messages(chat_id: ChatId, send_message: SendMessage):
     if not today_workers:
         await send_message(chat_id=chat_id, message=_("Nobody has joined the meeting!"), message_thread_id=topic_id)
     else:
+        today_usernames = []
         for user in today_workers:
-            for message in make_daily_messages(username=user.username):
-                await send_message(chat_id=chat_id, message=message, message_thread_id=topic_id)
+            today_usernames.append(f"@{user.username}")
+
+        msg_counter = 0
+        for message in make_daily_messages(usernames=" ".join(today_usernames)):
+            msg_counter += 1
+            msg_key = f"meeting_msg_id_{msg_counter}"
+
+            new_msg = await send_message(chat_id=chat_id, message=message, message_thread_id=topic_id)
+
+            setattr(chat_state, msg_key, new_msg.message_id)
+            await save_state(chat_state=chat_state)
 
 
 def make_job_id(some_id: int):
