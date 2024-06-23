3002a5a73f1d0b992f4d39b410eb48aa80b40fcd
kibertod <cyber704d@gmail.com>
2024-06-23 20:28:08 +0300

    conduct meetings every day

--- bot/constants.py
+++ bot/constants.py
@@ -17,8 +17,8 @@ iso8601 = "ISO 8601"
 
 datetime_time_format = "%H:%M %Y-%m-%d"
 
-day_of_week = "0-4"
+day_of_week = "0-6"
 
-day_of_week_pretty = "Monday - Friday"
+day_of_week_pretty = "Monday - Sunday"
 
 sample_time = "2024-06-03T14:00:00+03:00"
