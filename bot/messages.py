5634a9aa3acf6ba9da5e987eb2e7f2d1be83056e
N. Adagamov <adagamov05@mail.ru>
2024-07-01 15:25:48 +0300

    Add messages if user entered invalid interval

--- bot/messages.py
+++ bot/messages.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from textwrap import dedent
-from typing import List
+from typing import List, Tuple
 
 from aiogram import html
 from aiogram.utils.i18n import gettext as _
@@ -9,6 +9,7 @@ from aiogram.utils.i18n import gettext as _
 from .commands import bot_command_descriptions, bot_command_names
 from .constants import day_of_week_pretty
 from .language import Language
+from .intervals import Interval, InvalidTimeFormatException, InvalidIntervalException
 
 
 def bot_intro():
@@ -56,3 +57,23 @@ def make_daily_messages(usernames: str) -> List[str]:
             usernames=usernames
         ),
     ]
+
+
+def make_interval_validation_message(interval_str: str, tz: str) -> Tuple[bool, str]:
+    try:
+        interval = Interval.from_string(interval_str=interval_str, tz=tz)
+        msg = _("Successfully parsed interval: {interval}").format(interval=interval)
+        return True, msg
+    except InvalidTimeFormatException as e:
+        msg = _("Error: Invalid time format for '{time}'. {msg}").format(time=e.time_str, msg=e.message)
+        return False, msg
+    except InvalidIntervalException as e:
+        msg = _("Error: {msg} (start: {start}, end: {end})").format(
+            msg=e.message,
+            start=e.start_time,
+            end=e.end_time
+        )
+        return False, msg
+    except Exception as e:
+        msg = _("Error: An unexpected error occurred. {error}").format(error=str(e))
+        return False, msg
