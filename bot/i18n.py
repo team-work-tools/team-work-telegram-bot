9cb398d5ac60984aef62133fa493ad750393d39b
ElinaNotElina <adelkaravaeva@mail.ru>
2024-07-08 21:27:43 +0300

    feat(bot): add /get_report command

--- bot/i18n.py
+++
@@ -1,5 +0,0 @@
-from aiogram.utils.i18n import I18n
-from .language import Language
-
-i18n = I18n(path="locales", default_locale=str(Language.default), domain="messages")
-_ = i18n.gettext
--- scripts/up.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck disable=SC2148
-
-if ! [[ -f .venv/bin/pybabel ]]; then
-    poetry install --with dev
-fi
-
-./scripts/locales.sh
-
-docker compose up --build
