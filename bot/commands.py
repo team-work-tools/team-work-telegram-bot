dd363f670c70dfe2479bcfd578f3c602741d0f14
ElinaNotElina <adelkaravaeva@mail.ru>
2024-07-08 21:27:41 +0300

    feat(bot): add /get_report command

--- bot/commands.py
+++ bot/commands.py
@@ -27,6 +27,7 @@ class BotCommands(BaseModel):
     unschedule_personal_vacation: str
     # info
     get_chat_state: str
+    get_report: str
 
 
 class BotCommandNames(BotCommands):
@@ -56,6 +57,7 @@ bot_command_names = BotCommandNames(
     unschedule_personal_vacation="unschedule_personal_vacation",
     # info
     get_chat_state="get_chat_state",
+    get_report="get_report"
 )
 
 
@@ -90,4 +92,5 @@ def bot_command_descriptions() -> BotCommandDescriptions:
         unschedule_personal_vacation=_("Unschedule the personal vacation."),
         # info settings
         get_chat_state=_("Get the chat state stored by the bot."),
+        get_report=_("Get the report."),
     )
