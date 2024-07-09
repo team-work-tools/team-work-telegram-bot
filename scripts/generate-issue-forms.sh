a3f84a6c1f0fdb55171f762132884bc74f5077b3
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-07-09 19:48:21 +0300

    fix(issues): remove full stops

--- scripts/generate-issue-forms.sh
+++ scripts/generate-issue-forms.sh
@@ -10,7 +10,7 @@ jinja2 "$ISSUE_TEMPLATE_TEMPLATES_DIR"/template.yml > "$TEMPLATE_TMP"
 jinja2 \
     -D form_name='Scenario' \
     -D type='scenario' \
-    -D form_description='Propose a scenario.' \
+    -D form_description='Propose a scenario' \
     -D form_title='Scenario: {{scenario_description}}' \
     -D thanks='Thanks for proposing a new scenario!' \
     -D when_completed='This issue MUST be closed only when the Customer agrees the scenario is completed!' \
@@ -22,7 +22,7 @@ jinja2 \
 jinja2 \
     -D form_name='Task' \
     -D type='task' \
-    -D form_description='Create a task.' \
+    -D form_description='Create a task' \
     -D form_title='{{task_description}}' \
     -D when_completed='This issue MUST be closed when all its subtasks are completed.'\
     -o "$ISSUE_TEMPLATE_DIR"/task.yml \
