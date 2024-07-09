e4116c62809484cd9111646deb935b86678a362e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-07-09 17:56:16 +0300

    fix(issues): template values

--- scripts/generate-issue-forms.sh
+++ scripts/generate-issue-forms.sh
@@ -8,14 +8,19 @@ TEMPLATE_TMP=$(mktemp)
 jinja2 "$ISSUE_TEMPLATE_TEMPLATES_DIR"/template.yml > "$TEMPLATE_TMP"
 
 jinja2 \
+    -D form_name='Scenario' \
     -D type='scenario' \
+    -D form_description='Propose a scenario.' \
+    -D form_title='Scenario: {{ scenario name }}' \
     -D thanks='Thanks for proposing a new scenario!' \
     -D when_completed='This issue MUST be closed only when the Customer agrees the scenario is completed!'\
     -o "$ISSUE_TEMPLATE_DIR"/scenario.yml \
     "$TEMPLATE_TMP"
 
 jinja2 \
+    -D form_name='Task' \
     -D type='task' \
+    -D form_description='Create a task.' \
     -D when_completed='This issue MUST be closed when all its subtasks are completed.'\
     -o "$ISSUE_TEMPLATE_DIR"/task.yml \
     "$TEMPLATE_TMP"
