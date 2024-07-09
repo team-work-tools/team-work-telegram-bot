a47c116be75ca1ce5bc2341a010a480c3e245210
ElinaNotElina <adelkaravaeva@mail.ru>
2024-07-09 23:26:45 +0300

    feat(bot): introduce /get_report command

---
+++ bot/constants.py
@@ -0,0 +1,34 @@
+from aiogram import html
+
+ENCODING = "utf-8"
+
+
+class AppCommands:
+    print_bot_message = "print-bot-message"
+    print_meeting_messages = "print-meeting-messages"
+    start = "start"
+
+
+jobstore = "bot_jobs"
+
+time_url = "https://time.lol"
+
+iso8601 = "ISO 8601"
+
+datetime_time_format = "%H:%M %Y-%m-%d"
+
+day_of_week = "0-6"
+
+day_of_week_pretty = "Monday - Friday"
+
+day_of_week_to_num = {
+    "monday": 0,
+    "tuesday": 1,
+    "wednesday": 2,
+    "thursday": 3,
+    "friday": 4,
+    "saturday": 5,
+    "sunday": 6,
+}
+
+sample_time = "2024-06-03T14:00:00+03:00"
---
+++ bot/messages.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from datetime import datetime
+from textwrap import dedent
+from typing import List
+
+from aiogram import html
+from .i18n import _
+
+from .commands import bot_command_descriptions, bot_command_names
+from .constants import day_of_week_pretty
+from .language import Language
+
+
+def bot_intro():
+    return _(
+        """
+        I can help you conduct daily (or at least regular) meetings.
+
+        You can control me by sending these commands:
+        """
+    ).strip()
+
+
+def make_help_message() -> str:
+    command_names = bot_command_names
+    command_descriptions = bot_command_descriptions()
+
+    return dedent(
+        f"""
+        {bot_intro()}
+        
+        {html.bold(_("Global commands"))}
+        /{command_names.start} - {command_descriptions.start}
+        /{command_names.help} - {command_descriptions.help}
+        /{command_names.set_language} - {command_descriptions.set_language}
+        
+        {html.bold(_("Team settings commands"))}
+        /{command_names.set_meetings_time} - {command_descriptions.set_meetings_time}
+        
+        {html.bold(_("Personal settings commands"))}
+        /{command_names.set_personal_meetings_days} - {command_descriptions.set_personal_meetings_days}
+        /{command_names.set_reminder_period} - {command_descriptions.set_reminder_period}
+        /{command_names.join} - {command_descriptions.join}
+        /{command_names.skip} - {command_descriptions.skip}
+        
+        {html.bold(_("Chat information commands"))}
+        /{command_names.get_chat_state} - {command_descriptions.get_chat_state}
+        """
+    ).strip()
+
+
+def make_daily_messages(usernames: str) -> List[str]:
+    return [
+        _("What did you do last working day? {usernames}").format(usernames=usernames),
+        _("What will you do today? {usernames}").format(usernames=usernames),
+        _("What (if anything) is blocking your progress? {usernames}").format(
+            usernames=usernames
+        ),
+    ]
