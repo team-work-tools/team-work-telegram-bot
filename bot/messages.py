9f4fb632ce0acec2b6699ee35957b4908796f800
N. Adagamov <adagamov05@mail.ru>
2024-07-12 00:14:30 +0300

    feat(messages): add missing messages from scenario

--- bot/messages.py
+++ bot/messages.py
@@ -2,6 +2,7 @@ from textwrap import dedent
 from typing import List, Tuple
 
 from aiogram import html
+from aiogram.utils import markdown as fmt
 from aiogram.utils.i18n import gettext as _
 
 from .commands import bot_command_descriptions, bot_command_names
@@ -36,6 +37,7 @@ def make_help_message() -> str:
         {html.bold(_("Personal settings commands"))}
         /{command_names.set_personal_meetings_days} - {command_descriptions.set_personal_meetings_days}
         /{command_names.set_reminder_period} - {command_descriptions.set_reminder_period}
+        /{command_names.set_working_hours} - {command_descriptions.set_working_hours}
         /{command_names.join} - {command_descriptions.join}
         /{command_names.skip} - {command_descriptions.skip}
         
@@ -56,22 +58,37 @@ def make_daily_messages(usernames: str) -> List[str]:
 
 
 def make_interval_validation_message(interval_str: str, tz: str) -> Tuple[bool, str]:
-    new = "Please, reenter interval."
     try:
         interval = Interval.from_string(interval_str=interval_str, tz=tz)
-        msg = _("Successfully parsed interval: {interval}\n{new}").format(interval=interval, new=new)
+        msg = _("Successfully parsed interval: {interval}").format(interval=interval)
         return True, msg
     except InvalidTimeFormatException as e:
-        msg = _("Invalid time format for '{time}'. {msg}\n{new}").format(time=e.time_str, msg=e.message, new=new)
+        msg = _("Invalid time format for '{time}'. {msg}\n").format(time=e.time_str, msg=e.message)
         return False, msg
     except InvalidIntervalException as e:
-        msg = _("{msg} (start: {start}, end: {end})\n{new}").format(
+        msg = _("{msg} (start: {start}, end: {end}).\n").format(
             msg=e.message,
             start=e.start_time,
-            end=e.end_time,
-            new=new
+            end=e.end_time
         )
         return False, msg
     except Exception as e:
         msg = _("An unexpected error occurred. {error}").format(error=str(e))
         return False, msg
+
+
+def make_interval_editing_instruction() -> str:
+    instruction_text = "Send me the new interval in the hh:mm - hh:mm format."
+    example_text = "Example: "
+    note = "Notes:\nThe example provides an interval copyable by clicking or tapping it."
+
+    text = fmt.text(instruction_text, "\n", example_text, fmt.hcode("19:00 - 22:30"), "\n", note, sep="")
+    return text
+
+
+def make_interval_editing_error(error_msg: str) -> str:
+    again_text = "Press 'Enter again' to enter the interval again."
+    cancel_text = "Press 'Cancel' to cancel editing this interval."
+
+    text = fmt.text(error_msg, "\n", again_text, "\n", cancel_text, sep="")
+    return text
