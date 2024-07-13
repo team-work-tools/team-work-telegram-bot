afd99455cfe6f01ec2dc62941fc7b69994b08cfa
N. Adagamov <adagamov05@mail.ru>
2024-07-13 21:50:34 +0300

    feat(menu): add all commands' names and descriptions to Telegram command menu

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,7 @@ from typing import Optional
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
 from aiogram.utils.i18n import I18n
 from aiogram.utils.i18n.middleware import FSMI18nMiddleware
 from apscheduler.jobstores.memory import MemoryJobStore
@@ -11,6 +12,7 @@ from pytz import utc
 
 from . import db, handlers
 from .constants import jobstore
+from .commands import bot_command_names, bot_command_descriptions
 from .custom_types import ChatId, SendMessage
 from .meeting import schedule_meeting
 from .middlewares import GroupCommandFilterMiddleware
@@ -38,6 +40,20 @@ async def restore_scheduled_jobs(
                 scheduler=scheduler,
                 send_message=send_message,
             )
+
+
+async def set_default_commands(bot: Bot):
+    commands = []
+    descriptions = bot_command_descriptions()
+
+    attribute_names = vars(bot_command_names).keys()
+    for attr in attribute_names:
+        cmd_name = getattr(bot_command_names, attr)
+        cmd_descr = getattr(descriptions, attr)
+        cmd = BotCommand(command=cmd_name, description=cmd_descr)
+        commands.append(cmd)
+
+    await bot.set_my_commands(commands=commands)
 
 
 async def main(settings: Settings) -> None:
@@ -69,3 +85,5 @@ async def main(settings: Settings) -> None:
     await bot.delete_webhook(drop_pending_updates=True)
 
     await dp.start_polling(bot)
+
+    await set_default_commands(bot=bot)
