59615339ff3a24f9d1001ec94f463ac2f80022e3
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-07-20 21:35:52 +0300

    refactor(bot): remove unused import

--- bot/reminder.py
+++ bot/reminder.py
@@ -4,7 +4,6 @@ from typing import Optional
 
 from aiogram import Bot
 from aiogram.exceptions import TelegramBadRequest, TelegramForbiddenError
-from aiogram.utils.i18n import gettext as _
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
