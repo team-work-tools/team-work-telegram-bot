5439c8fe0fab750be543b5ca589e9938153fddfa
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-07-20 22:09:14 +0300

    fix(bot): apply i18n context for more updates

--- bot/i18n_middleware.py
+++ bot/i18n_middleware.py
@@ -1,4 +1,4 @@
-from aiogram.types import Message, TelegramObject, Update
+from aiogram.types import Message, TelegramObject, Update, CallbackQuery
 from aiogram.utils.i18n import I18nMiddleware
 
 from .language import Language
@@ -9,12 +9,20 @@ class MyI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: dict) -> str:
         match event:
             case Update():
-                match message := event.message:
+                match event.message:
                     case Message():
-                        chat_state = await load_state(
-                            message.chat.id,
-                            message.is_topic_message,
-                            message.message_thread_id,
-                        )
-                        return str(chat_state.language)
+                        event_message = event.message
+                match callback_query := event.callback_query:
+                    case CallbackQuery():
+                        match callback_query.message:
+                            case Message():
+                                event_message = callback_query.message
+        match message := event_message:
+            case Message():
+                chat_state = await load_state(
+                    message.chat.id,
+                    message.is_topic_message,
+                    message.message_thread_id,
+                )
+                return str(chat_state.language)
         return str(Language.default)
